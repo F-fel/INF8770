3356a95b6ecf02dba29777ff188bf42f746effe5
SpongeyBob <farid.elfakhry@outlook.com>
2022-01-28 12:12:32 -0500

    updated lz77, not working like exxamples
    issue may be in findRepetition

--- TP1/LZ77.py
+++ TP1/LZ77.py
@@ -1,70 +1,73 @@
-tailleDict = 4 # NUMBER OF CHAR IN MEMORY
-def encode(message):
-    triplets = []  # Pour sauvegarder les triplets
-    index = 0  # Position courante dans le message
-    while index < len(message):
-        decalage = 0
-        symbDict = message[max(0, index - tailleDict): index]  # Symboles disponibles du dictionnaire
-        if len(symbDict) < tailleDict:
-          decalage = tailleDict - len(symbDict) #Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
-        print(symbDict)
-        sousChaine = message[index:len(message)-1] #Le dernier caractère sera ajouté comme 3e élément du dernier triplet
+import math
+from pickle import FALSE
+class LZ77:
+  """
+  Une implementation de l'algorithme LZ77
+  """
+  MAX_WINDOW_SIZE = 511
+  MAX_LOOKAHEAD_SIZE = 63
 
-        # On cherche la sous-chaine la plus longue
-        pos, length = 0, 0
-        while len(sousChaine) > 0:
-            if sousChaine in symbDict:
-                pos, length = symbDict.rfind(sousChaine), len(sousChaine) #dernière occurrence si plusieurs choix
-                break
-            sousChaine = sousChaine[:-1] # On a pas trouvé, donc on enlève un élément.
+  def __init__(self, lookaheadBuffer =15, window=20):
+    self.bufferSize = min(self.MAX_LOOKAHEAD_SIZE, lookaheadBuffer)
+    self.windowSize = min(self.MAX_WINDOW_SIZE, window)
 
-        # Regarde ensuite si on peut allonger la sous-chaine trouvée après la position de l'index: AB|ABABABABAB
-        increment = 0
-        while length > 0 \
-                and index+length+increment < (len(message)-1) \
-                and message[index-len(symbDict)+pos+length+increment] == message[index+length+increment]:
-            increment += 1
-        length += increment
+  def compress(self, data, verbose=False):
+    """
+    compression utilisant LZ77 et les parametres definis dans la classe
+    data: Les donnees a compresser
+    verbose: faire des print en mi chemin (utile pour le debogage)
+    """
+    triplets = []
+    i=0
+    while i < len(data):
+      # definir le dicitonnaire pour l'iteration
+      dict = data[i-self.windowSize:i] if i > self.windowSize else data[0:i]
+      if verbose : print(" dict =", dict)
+      #definir le buffer
+      buffer = data[i:i+self.bufferSize] if i+self.bufferSize < len(data) - 1 else data[i:-1]
+      if verbose: print("buffer = ", buffer)
+      match = self.findLongestMatch(buffer,dict)
+      if verbose:
+         print(match) if len(match)>0 else print("N/A")
+      if len(match) > 0 :
+        length = self.findRepetition(buffer,match) * len(match)
+        triplets.append((dict.find(match),length,data[i+length]))
+        i+=length+1
+      else:
+        triplets.append((0,0,data[i]))
+        i+=1
+      if verbose : print(triplets)
+    return triplets
+  def findLongestMatch(self, buffer, dict):
+    """trouve la plus longue chaine de character qui se trouve dans dict on utilise juste le buffer est pas tout le data"""
+    match = buffer
+    while match not in dict and len(match)!= 0:
+      match = match[:-1]
+    return match
 
-        # Enregistrement des triplets
-        c = message[index + length]  #Caractère suivant non encodé.
+  def findRepetition(self,buffer,match):
+    """trouve le nombre de fois que match est repete dans buffer (back to back)"""
+    startIndex = len(match)
+    prediction = len(match)
+    repetitionCounter =1
+    while prediction < len(buffer) \
+      and buffer.find(match, startIndex ) == prediction:
+      prediction += len(match)
+      startIndex += len(match)
+      repetitionCounter +=1
+    return repetitionCounter
+  def decompress(self,triplets):
+    retval =""
+    for triplet in triplets:
+      index ,length,nextchar = triplet[0],triplet[1],triplet[2]
+      dict = retval[- self.windowSize: ] if self.windowSize < len(retval) else retval
+      if (index , length) == (0,0):
+        retval += nextchar
+      else:
+        a =0
 
-        if length == 0:
-          pos = 0
-        else:
-          pos = pos + decalage #Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
-
-        triplets.append((pos, length, c))
-
-        index += max(length+1, 1)  # Avance la position dans le message
-
-        print(triplets)
-
-        
 
 if __name__ == "__main__":
-    str = "ABBACDAABAABAABCDA"
-    encode(str)
-
-
-##  
-##  index = 0  # Position courante dans le message
-##  while index < len(message):
-##      decalage = 0
-##      symbDict = message[max(0, index - tailleDict): index]  # Symboles disponibles du dictionnaire
-##      if len(symbDict) < tailleDict: 
-##        decalage = tailleDict - len(symbDict)#Pour l'ajustement des indices de position quand le dictionnaire n'est pas plein.
-##      print(symbDict)
-##      sousChaine = message[index:len(message)-1] #Le dernier caractère sera ajouté comme 3e élément du dernier triplet
-##  
-##      # On cherche la sous-chaine la plus longue
-##      pos, length = 0, 0
-##      while len(sousChaine) > 0:
-##          if sousChaine in symbDict:
-##              pos, length = symbDict.rfind(sousChaine), len(sousChaine) #dernière occurrence si plusieurs choix
-##              break
-##          sousChaine = sousChaine[:-1] # On a pas trouvé, donc on enlève un élément.
-##       
-##      # Regarde ensuite si on peut allonger la sous-chaine trouvée après la position de l'index: AB|ABABABABAB
-##      increment = 0
-##      print(triplets)
+    str = "CBAAAAAAAAAAAAAAAAAAAAABAABAACD"
+    lz = LZ77(22,6)
+    print(lz.compress(str, verbose=True))
