1224e61f3a9a5e5b24ec4d25ff042dacc8d02dd3
SpongeyBob <farid.elfakhry@outlook.com>
2022-02-02 10:49:45 -0500

    added time satistics

--- TP1/LewisHamilton.py
+++ TP1/LewisHamilton.py
@@ -4,6 +4,7 @@ This is the driver for our project
 import csv
 import os
 import random
+from time import process_time_ns
 from generator import Generator
 from RLE import RLE
 from LZ77 import LZ77
@@ -13,6 +14,7 @@ DATA_DIR = "DATA"
 LZ_DIR = "LZ77"
 RLE_DIR = "RLE"
 sampleFactory = Generator(255)
+stats = {}
 # compressors
 compressorRLE = RLE()
 compressorLZ  = LZ77()
@@ -43,52 +45,63 @@ def generateSimpleSamples():
     file.write(txt)
     file.close()
 
-def compress():
+def compressRLE(filename, data):
+    tStart = process_time_ns()
+    compressorRLE.compress(data)
+    tEnd = process_time_ns()
+    #save
+    rleFile = open(RLE_DIR+'/'+filename+".crle",'wb')
+    rleFile.write(compressorRLE.toByte())
+    rleFile.close()
+    #return time used
+    return tEnd-tStart
+def compressLZ(filename, data):
+    tStart = process_time_ns()
+    lzCompressed  = compressorLZ.compress(data)
+    tEnd = process_time_ns()
+    #save
+    lzFile = open(LZ_DIR + '/' +filename+".lz77", 'wb')
+    lzFile.write(compressorLZ.tobyte(lzCompressed))
+    lzFile.close()
+    #return time used
+    return tEnd-tStart
+def getStats():
     for filename in os.listdir(DATA_DIR):
+        size = os.path.getsize(DATA_DIR + '/'+filename)
+        stats[filename].append(size)
+    for filename in os.listdir(RLE_DIR):
+        size = os.path.getsize(RLE_DIR + '/'+filename)
+        filename = filename.replace(".crle","")
+        stats[filename].append(size)
+    for filename in os.listdir(LZ_DIR):
+        size = os.path.getsize(LZ_DIR + '/'+filename)
+        filename = filename.replace(".lz77","")
+        stats[filename].append(size)
+
+
+if __name__ == "__main__":
+
+    generateRandomSamples(10)
+    generateSimpleSamples()
+    for filename in os.listdir(DATA_DIR):
+        #add the file to the list
+        stats[filename] = []
         # read and encode data
         file = open(DATA_DIR +'/'+filename,'rb')
         rawdata = bytearray(file.read())
         data = ""
         for octets in rawdata:
             data+=chr(octets)
-        #compress the data
-        lzCompressed  = compressorLZ.compress(data)
-        compressorRLE.compress(data)
         file.close()
-        #save RLE
-        rleFile = open(RLE_DIR+'/'+filename,'wb')
-        rleFile.write(compressorRLE.toByte())
-        rleFile.close()
-        #save LZ77
-        lzFile = open(LZ_DIR + '/' +filename+".lz77", 'wb')
-        lzFile.write(compressorLZ.tobyte(lzCompressed))
-        lzFile.close()
-def getStats():
-    stats = {}
-    for filename in os.listdir(DATA_DIR):
-        stats[filename] = []
-        size = os.path.getsize(DATA_DIR + '/'+filename)
-        stats[filename].append(size)
-    for filename in os.listdir(RLE_DIR):
-        size = os.path.getsize(RLE_DIR + '/'+filename)
-        #filename = filename.replace(".crle","")
-        stats[filename].append(size)
-    for filename in os.listdir(LZ_DIR):
-        size = os.path.getsize(LZ_DIR + '/'+filename)
-        filename = filename.replace(".lz77","")
-        stats[filename].append(size)
+        stats[filename].append(compressRLE(filename, data))
+        stats[filename].append(compressLZ(filename, data))
+    getStats()
     with open('stats.csv', 'w',newline='') as csv_file:  
         writer = csv.writer(csv_file)
-        writer.writerow(["file name","original value","RLE value","LZ77 value","rle ratio", "LZ77 ratio"])
+        writer.writerow(["file name","RLE time ","LZ77 time","original size","RLE size","LZ77 size","rle ratio", "LZ77 ratio"])
         for key, value in stats.items():
-            og = value[0]
-            rle=value[1]
-            lz=value[2]
-            writer.writerow([key, og,rle,lz,og/rle,og/lz])
-
-if __name__ == "__main__":
-    generateRandomSamples(10)
-    generateSimpleSamples()
-    compress()
-    getStats()
+            og = value[2]
+            rle=value[3]
+            lz=value[4]
+            writer.writerow([key,value[0], og,rle,lz,og/rle,og/lz])
     print("DONE")
